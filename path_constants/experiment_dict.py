6cf6274cb1e915581acd61b8753d4642c5c25b28
Nadya Chernyavskaya <nadezda.chernyavskaya@gmail.com>
2021-08-09 17:28:09 +0200

    path to nadya dirs

--- path_constants/experiment_dict.py
+++ path_constants/experiment_dict.py
@@ -3,17 +3,17 @@ path_dict = {
 
     # VAE directory patterns (TODO: fix run str)
 	
-	'model_dir' : '/eos/home-k/kiwoznia/data/VAE_models/$run$',
-	'analysis_base_dir_bin_count' : '/eos/home-k/kiwoznia/data/VAE_results/bump_hunt_results/$run$/bin_count/$strategy$/$quantile$',
-	'model_analysis_base_dir': '/eos/home-k/kiwoznia/data/VAE_results/model_analysis/$run$',
-	'model_comparison_dir': '/eos/home-k/kiwoznia/data/VAE_results/model_analysis/$run1$_vs_$run2$',
+	'model_dir' : '/eos/user/n/nchernya/MLHEP/AnomalyDetection/autoencoder_for_anomaly/graph_based/VAE_models/$run$',
+	'analysis_base_dir_bin_count' : '/eos/user/n/nchernya/MLHEP/AnomalyDetection/autoencoder_for_anomaly/graph_based/VAE_results/bump_hunt_results/$run$/bin_count/$strategy$/$quantile$',
+	'model_analysis_base_dir': '/eos/user/n/nchernya/MLHEP/AnomalyDetection/autoencoder_for_anomaly/graph_based/VAE_results/model_analysis/$run$',
+	'model_comparison_dir': '/eos/user/n/nchernya/MLHEP/AnomalyDetection/autoencoder_for_anomaly/graph_based/VAE_results/model_analysis/$run1$_vs_$run2$',
 	
     # bump hunt results pattern: run_x/fig/loss_u
 
-    'analysis_base_dir_fig' : '/eos/home-k/kiwoznia/data/VAE_results/bump_hunt_results/$run$/fig/loss_$loss_strat$',
+    'analysis_base_dir_fig' : '/eos/user/n/nchernya/MLHEP/AnomalyDetection/autoencoder_for_anomaly/graph_based/VAE_results/bump_hunt_results/$run$/fig/loss_$loss_strat$',
 
     # QR directory pattern: run_x/sig_y/xsec_z/loss_u
 
-    'model_dir_qr': '/eos/home-k/kiwoznia/data/QR_models/run_$run$',
-    'analysis_base_dir_qr': '/eos/user/k/kiwoznia/data/QR_results/analysis/run_$run$/sig_$sig_name$/xsec_$sig_xsec$/loss_$loss_strat$',
+    'model_dir_qr': '/eos/user/n/nchernya/MLHEP/AnomalyDetection/autoencoder_for_anomaly/graph_based/QR_models/run_$run$',
+    'analysis_base_dir_qr': '/eos/user/n/nchernya/MLHEP/AnomalyDetection/autoencoder_for_anomaly/graph_based/QR_results/analysis/run_$run$/sig_$sig_name$/xsec_$sig_xsec$/loss_$loss_strat$',
 }
